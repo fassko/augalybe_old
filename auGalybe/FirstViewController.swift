82656b8e74c496c59765778390b0afd4f9aabf8f
Kristaps Grinbergs <fassko@gmail.com>
2019-08-30 16:01:54 +0300

    Initial Commit

---
+++ auGalybe/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  auGalybe
+//
+//  Created by Kristaps Grinbergs on 30/08/2019.
+//  Copyright © 2019 fassko. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
